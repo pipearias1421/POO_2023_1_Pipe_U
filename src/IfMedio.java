8cb8082f1eda35ad767507ee08c19089cd2b13ec
pipearias1421 <luis.arias6126@udecaldas.edu.co>
2023-03-22 21:17:59 -0500

    segundo commit

--- src/IfMedio.java
+++ src/IfMedio.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-
-public class IfSupletorio {
+public class IfMedio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int nota1=0;
 		int nota2=0;
 		int definitiva=0;
+		
 		
 		Scanner entrada = new Scanner(System.in);
 		
@@ -19,11 +19,10 @@ public class IfSupletorio {
 		definitiva=nota1 + nota2;
 		if(definitiva>=7) {
 			System.out.println("Aprueba la materia");
-		}else if(definitiva>=5 && definitiva<7) {
-			System.out.println("Supletorio");
-			}else {
-				System.out.println("no apruebas");
-			}
+		}else {
+			System.out.println("No apruebas");
+		}
+		
 	}
 
 }
